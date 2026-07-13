cb54427f7775883d6c5a1db3048b604fe66fa1c3
m7-code <mughiraasad6@gmail.com>
2026-07-13 03:53:50 -0700

    improved

--- api/server.js
+++ api/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
